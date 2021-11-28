e63ebd92a7c9bf9f9e562545e9711ae79a3e8b17
Minh Nguyen <hongminh1620@gmail.com>
2021-11-28 18:53:05 +0000

    fix: Add vote as attribute of options table

--- app/models/options.py
+++ app/models/options.py
@@ -11,6 +11,7 @@ class OptionModel(db.Model):
     description = db.Column(db.String(200), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     box_id = db.Column(db.Integer, db.ForeignKey("boxes.id"), nullable=False)
+    vote = db.Column(db.String(100), nullable=False)
 
     def __init__(self, name, description, box_id, user_id):
         self.name = name
