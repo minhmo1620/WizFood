f62d81d680ae8c47a4bffcf96fcadf1888b6715c
Minh Nguyen <hongminh1620@gmail.com>
2021-11-28 18:51:55 +0000

    Fix the argument name

--- app/schemas/votes.py
+++ app/schemas/votes.py
@@ -4,5 +4,4 @@ from .bases import Base
 
 
 class VoteSchema(Base):
-    votes = fields.Dict(key=fields.Integer(), values=fields.Integer(validate=validate.Range(min=0, max=2)))
-
+    votes = fields.Dict(keys=fields.Integer(), values=fields.Integer(validate=validate.Range(min=0, max=2)))
