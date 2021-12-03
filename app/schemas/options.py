557946a541f64080d1345a410e52f887aa4ed717
Minh Nguyen <hongminh1620@gmail.com>
2021-12-03 23:23:28 +0000

    Add vote data to schema

--- app/schemas/options.py
+++ app/schemas/options.py
@@ -1,4 +1,6 @@
-from marshmallow import fields, validate
+import json
+
+from marshmallow import fields, validate, post_dump
 
 from .bases import Base
 
@@ -7,3 +9,9 @@ class OptionSchema(Base):
     name = fields.Str(required=True, validate=validate.Length(max=100, min=1))
     description = fields.Str(required=True, validate=validate.Length(max=200, min=1))
     id = fields.Int(required=False)
+    vote = fields.Str(required=False)
+
+    @post_dump
+    def load_vote(self, data, **kwargs):
+        data['vote'] = json.loads(data['vote'])
+        return data
