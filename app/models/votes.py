e658ea51b3229c3b520d1bf0aefd519bc42ca1ae
Minh Nguyen <hongminh1620@gmail.com>
2021-12-08 21:05:33 +0000

    Update the VoteModel to add box_id

--- app/models/votes.py
+++ app/models/votes.py
@@ -6,9 +6,11 @@ class VoteModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    box_id = db.Column(db.Integer, db.ForeignKey("boxes.id"), nullable=False)
     # TODO: store option_id and box_id for future reference
     data = db.Column(db.String, nullable=False)
 
-    def __init__(self, user_id, data):
+    def __init__(self, user_id, box_id, data):
         self.user_id = user_id
+        self.box_id = box_id
         self.data = data
