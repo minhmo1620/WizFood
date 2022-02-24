dc08b04afd247cb5eb66ad80975b390bc71ff635
Minh Nguyen <hongminh1620@gmail.com>
2022-02-24 20:12:37 +0700

    Change the URI to different paths

--- app/configs/local.py
+++ app/configs/local.py
@@ -4,5 +4,4 @@ from app.configs.base import Config
 
 
 class LocalConfig(Config):
-    db_path = os.path.join(os.path.dirname(__file__), 'database.db')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(db_path)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
