12d289d2adf21d7d45fc0ec3e0988c008e8c9280
Minh Nguyen <hongminh1620@gmail.com>
2022-02-25 18:20:51 +0700

    Delete ConversationsSchema and update UpdateConversationSchema by deleting username

--- app/schemas/conversations.py
+++ app/schemas/conversations.py
@@ -3,13 +3,6 @@ from marshmallow import fields, validate
 from .bases import Base
 
 
-class ConversationSchema(Base):
-    username = fields.Str(required=True,
-                          validate=validate.Length(max=80, min=1))
-
-
 class UpdateConversationSchema(Base):
-    username = fields.Str(required=True,
-                          validate=validate.Length(max=80, min=1))
     answer = fields.Str(required=True,
                           validate=validate.Length(max=80, min=1))
