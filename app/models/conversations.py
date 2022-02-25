26a5ca8401646578ffb69fa8eed9698631b6e9f9
Minh Nguyen <hongminh1620@gmail.com>
2022-02-25 18:20:12 +0700

    Change the conversations table to link with userid

--- app/models/conversations.py
+++ app/models/conversations.py
@@ -7,11 +7,11 @@ class ConversationModel(db.Model):
     __tablename__ = "conversations"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     answers = db.Column(db.String(100))
     ended = db.Column(db.Boolean)
 
-    def __init__(self, username):
-        self.username = username
+    def __init__(self, user_id):
+        self.user_id = user_id
         self.answers = json.dumps([])
         self.ended = 0
