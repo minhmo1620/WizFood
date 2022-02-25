0a1db3dd3fdf65429231e81250e9529204d57b4a
Minh Nguyen <hongminh1620@gmail.com>
2022-02-25 17:43:38 +0700

    Change request method for vote API

--- app/controllers/votes.py
+++ app/controllers/votes.py
@@ -1,5 +1,5 @@
 import json
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 from app import db
 
 from app.controllers.helpers import validate_box_id
@@ -25,7 +25,7 @@ def get_vote_data(user_id, box_id):
     return jsonify({}), 200
 
 
-@votes_blueprint.route("/boxes/<int:box_id>/vote", methods=["POST"])
+@votes_blueprint.route("/boxes/<int:box_id>/vote", methods=["POST", "PUT"])
 @token_required
 @validate_input(schema=VoteSchema)
 def vote_options(user_id, box_id, data):
@@ -57,7 +57,28 @@ def vote_options(user_id, box_id, data):
     # Get value of old vote
     vote = db.session.query(VoteModel).filter(VoteModel.box_id == box_id).filter(VoteModel.user_id == user_id).first()
 
-    if vote:
+    if request.method == "POST":
+        if vote:
+            return jsonify({"message": "Please choose the correct method"}), 400
+
+        for option in all_options:
+            option_id = str(option.id)
+            # Update the vote value
+            vote_value = data[option_id]
+            option_vote_value = json.loads(option.vote)
+            option_vote_value[vote_value] += 1
+            option.vote = json.dumps(option_vote_value)
+        db.session.commit()
+
+        new_vote = VoteModel(user_id, box_id, json.dumps(data))
+        db.session.add(new_vote)
+        db.session.commit()
+        return jsonify({"message": "Voted successfully"}), 201
+
+    elif request.method == "PUT":
+        if not vote:
+            return jsonify({"message": "Please choose the correct method"}), 400
+        
         vote_data = json.loads(vote.data)
         for option in all_options:
             option_id = str(option.id)
@@ -77,18 +98,3 @@ def vote_options(user_id, box_id, data):
         vote.data = json.dumps(data)
         db.session.commit()
         return jsonify({"message": "Update vote successfully"}), 200
-
-    else:
-        for option in all_options:
-            option_id = str(option.id)
-            # Update the vote value
-            vote_value = data[option_id]
-            option_vote_value = json.loads(option.vote)
-            option_vote_value[vote_value] += 1
-            option.vote = json.dumps(option_vote_value)
-        db.session.commit()
-
-        new_vote = VoteModel(user_id, box_id, json.dumps(data))
-        db.session.add(new_vote)
-        db.session.commit()
-        return jsonify({"message": "Voted successfully"}), 201
