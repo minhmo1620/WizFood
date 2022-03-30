d2bd0de044f5ac1d29e383bb78e3089c93e6bca3
Minh Nguyen <hongminh1620@gmail.com>
2022-03-30 13:51:36 -0700

    Delete irrelevant docstrings

--- app/controllers/foods.py
+++ app/controllers/foods.py
@@ -6,7 +6,7 @@ from app.models.knowledgebases import KnowledgeBaseModel
 from app.helpers import validate_input, token_required
 
 """
-In each box, user can add their food recommendation as an option for everyone to vote later
+
 """
 foods_blueprint = Blueprint("foods", __name__)
 
@@ -15,7 +15,7 @@ foods_blueprint = Blueprint("foods", __name__)
 @token_required
 def get_foods(user_id):
     """
-    Get all options of one box based on box_id
+
     """
     pass
 
@@ -24,6 +24,6 @@ def get_foods(user_id):
 @token_required
 def create_new_option(user_id, data):
     """
-    Create new option in one specific box
+
     """
     pass
