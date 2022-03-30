7365b81e6536d524d9b0b44314778e54db165c8d
Minh Nguyen <hongminh1620@gmail.com>
2022-03-30 13:50:02 -0700

    Rename the model

--- app/models/knowledgebases.py
+++ app/models/knowledgebases.py
@@ -2,11 +2,11 @@ from ..db import db
 
 
 class KnowledgeBaseModel(db.Model):
-    __tablename__ = "knowledge_base"
+    __tablename__ = "knowledgebases"
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    kb =  db.Column(db.Text(4294000000), nullable=False)
+    kb = db.Column(db.Text(4294000000), nullable=False)
 
     def __init__(self, user_id):
         self.user_id = user_id
