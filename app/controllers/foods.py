e1675fc5c8cf8086527ec4ba556b08e47b43be98
Minh Nguyen <hongminh1620@gmail.com>
2022-03-30 22:17:54 -0700

    Add endpoint to add new food to the KB

--- app/controllers/foods.py
+++ app/controllers/foods.py
@@ -1,8 +1,10 @@
+import json
 from flask import Blueprint, jsonify
 
 from app import db
 from app.models.foods import FoodModel
 from app.models.knowledgebases import KnowledgeBaseModel
+from app.schemas.foods import FoodSchema
 from app.helpers import validate_input, token_required
 
 """
@@ -12,7 +14,7 @@ foods_blueprint = Blueprint("foods", __name__)
 
 
 @foods_blueprint.route("/foods", methods=["GET"])
-@token_required
+# @token_required
 def get_foods(user_id):
     """
 
@@ -21,9 +23,29 @@ def get_foods(user_id):
 
 
 @foods_blueprint.route("/foods", methods=["POST"])
-@token_required
-def create_new_option(user_id, data):
+# @token_required
+@validate_input(schema=FoodSchema)
+def create_new_food(data):
     """
 
     """
-    pass
+    user_id = 1
+    food_data = {
+        "name": data["name"],
+        "preference": data["preference"],
+        "origin": data["origin"],
+        "ingredients": data["ingredients"],
+        "vegeterian": "yes" if data["vegeterian"] else "no",
+        "cooking_method": data["cooking_method"],
+        "calories": data["calories"]
+    }
+
+    # Check existing food
+    food = db.session.query(FoodModel).filter(FoodModel.user_id == user_id and FoodModel.name==data["name"]).first()
+    if food:
+        return jsonify({"message": "Existed food"}), 400
+
+    new_food = FoodModel(user_id, data["name"], json.dumps(food_data))
+    db.session.add(new_food)
+    db.session.commit()
+    return jsonify({"message": "Added the food to the knowledge base successfully"}), 201
