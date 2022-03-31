f6c4364eafa7a17326ba7fcf702b9959dfaa354f
Minh Nguyen <hongminh1620@gmail.com>
2022-03-30 19:28:05 -0700

    Modify create_*_questions function to accept question as params and add get_questions_dict based on user_id

--- model_helpers.py
+++ model_helpers.py
@@ -8,52 +8,11 @@ def create_KB(user_id):
     knowledgebase = db.session.query(KnowledgeBaseModel).filter_by(user_id=user_id).first()
     return knowledgebase.update_kb()
 
-def create_food_KB(food):
-    name = food["name"]
-    food_str = f"\nfood({name}) :- "
-    for k, v in food.items():
-        if k == "name" or k == "calories":
-            continue
-        elif k == "ingredients":
-            for ingredient in v:
-                food_str += f"\n    {ingredient}(yes),"
-        else:
-            food_str += f"\n    {k}({v}),"
-    
-    food_str = food_str[:-1] + ".\n"
-    return food_str
+def get_questions_dict(user_id):
+    knowledgebase = db.session.query(KnowledgeBaseModel).filter_by(user_id=user_id).first()
+    return knowledgebase.get_questions_dict()
 
-def create_categories_dict(food_data):
-    str_dict = "{"
-    for food in food_data:
-        name = food["name"]
-        calories = food["calories"]
-        str_dict += f"\n    {name}: {calories},"
-    str_dict = str_dict[:-1] + " \n}"
-    return f"\nmy_dict(_{str_dict}).\n"
-
-def create_askables(askable_dict):
-    askables = "\n% Askables\n"
-    for k, v in askable_dict.items():
-        if v["type"] == "numberask":
-            askables += f"{k}(X) :- numberask({k}, X).\n"
-        elif v["type"] == "menuask":
-            choices = convert_list_to_str(v["choices"])
-            askables += f"{k}(P):- menuask({k}, P, {choices}).\n"
-        elif v["type"] == "ask":
-            askables += f"{k}(X) :- ask({k}, X).\n"
-    return askables
-
-def convert_list_to_str(a):
-    res = "["
-    for i in a:
-        res += i
-        res += ', '
-    res = res[:-2] + ']'
-    return res
-
-
-def create_ask_question(A, V):
+def create_ask_question(A, V, question):
     """
     Create question to ask users includes 
     - questions for askables
@@ -69,7 +28,7 @@ def create_ask_question(A, V):
         "options": ["yes", "no"]
     })
     
-def create_numberask_question(A):
+def create_numberask_question(A, question):
     """
     Create questions to ask for number
     """
@@ -79,7 +38,7 @@ def create_numberask_question(A):
         "options": []
     })
 
-def create_menuask_question(A, menu):
+def create_menuask_question(A, menu, question):
     """
     Create questions to have menuasks
     """
