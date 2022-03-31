beafaba96e380411d606b0ea98ecffefc52b9049
Minh Nguyen <hongminh1620@gmail.com>
2022-03-31 14:36:01 -0700

    Increase limit for data column

--- app/models/foods.py
+++ app/models/foods.py
@@ -7,7 +7,7 @@ class FoodModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     name = db.Column(db.String(100), nullable=False)
-    data =  db.Column(db.String(100), nullable=False)
+    data =  db.Column(db.String(300), nullable=False)
 
     def __init__(self, user_id, name, data):
         self.user_id = user_id
