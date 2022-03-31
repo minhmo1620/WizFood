18b0feafda8f9dc40b9c2bd73c65b33dbb081dc5
Minh Nguyen <hongminh1620@gmail.com>
2022-03-30 17:08:47 -0700

    Delete size of Textto work with Postgres

--- app/models/knowledgebases.py
+++ app/models/knowledgebases.py
@@ -9,13 +9,13 @@ class KnowledgeBaseModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    kb = db.Column(db.Text(4294000000), nullable=False)
-    foods = db.Column(db.Text(4294000000), nullable=False)
-    askable_dict = db.Column(db.Text(4294000000), nullable=False)
+    kb = db.Column(db.Text, nullable=False)
+    foods = db.Column(db.Text, nullable=False)
+    askable_dict = db.Column(db.Text, nullable=False)
     
-    kb_header = db.Column(db.Text(5000), nullable=False)
-    calories_rules = db.Column(db.Text(5000), nullable=False)
-    rules = db.Column(db.Text(5000), nullable=False)
+    kb_header = db.Column(db.Text, nullable=False)
+    calories_rules = db.Column(db.Text, nullable=False)
+    rules = db.Column(db.Text, nullable=False)
 
     def __init__(self, user_id):
         self.user_id = user_id
