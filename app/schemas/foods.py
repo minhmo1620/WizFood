0ad138d56910b7876b373ef43d7e650d1cc186ab
Minh Nguyen <hongminh1620@gmail.com>
2022-04-01 13:37:13 -0700

    Change the schema of food to require only name and ingredients

--- app/schemas/foods.py
+++ app/schemas/foods.py
@@ -8,11 +8,10 @@ from .bases import Base
 class FoodSchema(Base):
     name = fields.Str(required=True, validate=validate.Length(max=100, min=1))
     ingredients = fields.Str(required=True, validate=validate.Length(max=200, min=1))
-    vegeterian = fields.Bool(required=True)
-    origin = fields.Str(required=True, validate=validate.Length(max=200, min=1))
-    preference = fields.Str(required=True, validate=validate.Length(max=200, min=1))
-    cooking_method = fields.Str(required=True, validate=validate.Length(max=200, min=1))
-    calories = fields.Int(required=True)
+    origin = fields.Str(required=False, validate=validate.Length(max=200, min=1))
+    preference = fields.Str(required=False, validate=validate.Length(max=200, min=1))
+    cooking_method = fields.Str(required=False, validate=validate.Length(max=200, min=1))
+    calories = fields.Int(required=False)
 
     @post_load
     def preprocess_ingredients(self, data, **kwargs):
@@ -20,11 +19,16 @@ class FoodSchema(Base):
         ingredients_list = [i.strip() for i in list(data["ingredients"].split(","))]
 
         for ingredient in ingredients_list:
-            ingredient_name = '_'.join(ingredient.split(" ")) if len(ingredient.split(" ")) > 0 else ingredient
+            ingredient_name = format_data(ingredient)
             tmp.append(ingredient_name)
 
         data["ingredients"] = tmp
-        data["name"] = data["name"].lower().strip().replace(" ", "_")
+        data["name"] = format_data(data["name"])
+
+        for k in ["origin", "preference", "cooking_method"]:
+            if k in data:
+                data[k] = format_data(data[k])
         return data
         
-
+def format_data(data):
+    return data.lower().strip().replace(" ", "_")
