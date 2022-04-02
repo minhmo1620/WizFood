6b61deaf11a16949a65650ab000381415b965c8a
Minh Nguyen <hongminh1620@gmail.com>
2022-04-01 22:16:11 -0700

    Add comment for helper function

--- app/controllers/helpers.py
+++ app/controllers/helpers.py
@@ -3,5 +3,10 @@ from app.models.boxes import BoxModel
 
 
 def validate_box_id(box_id):
+    """
+    Validate the box_id. Check if the box exist
+    Input:
+        - box_id (int) The ID of the box
+    """
     box = db.session.query(BoxModel).filter(BoxModel.id == box_id).first()
     return True if (box is not None) else False
